99a537b625ac87445964f82e539c9750f746816d
Timothy Gu <timothygu99@gmail.com>
2015-04-02 15:37:03 -0700

    Use superagent

--- repos.js
+++ repos.js
@@ -4,7 +4,7 @@
  * MIT-licensed
  */
 
-var https = require('https')
+var request = require('superagent')
   , bl = require('bl')
   , ReadWriteLock = require('rwlock')
   , lock = new ReadWriteLock()  // Make sure the data is cached
@@ -18,37 +18,29 @@ module.exports = function (cb) {
       return rr()
     }
     lock.writeLock(function (rw) {
-      https.get({
-        host: 'api.github.com'
-      , path: '/orgs/jstransformers/repos'
-      , headers: {
-          // https://developer.github.com/v3/#user-agent-required:
-          //
-          // > All API requests MUST include a valid `User-Agent` header.
-          // > Requests with no `User-Agent` header will be rejected. We
-          // > request that you use your GitHub username, or the name of your
-          // > application, for the `User-Agent` header value. This allows us
-          // > to contact you if there are problems.
-          'User-Agent': 'jstransformers'
-        }
-      }, function (res) {
-        res.pipe(bl(function (error, data) {
-          if (error){
+      request
+        .get('https://api.github.com/orgs/jstransformers/repos')
+        // https://developer.github.com/v3/#user-agent-required:
+        //
+        // > All API requests MUST include a valid `User-Agent` header.
+        // > Requests with no `User-Agent` header will be rejected. We
+        // > request that you use your GitHub username, or the name of your
+        // > application, for the `User-Agent` header value. This allows us
+        // > to contact you if there are problems.
+        .set('User-Agent', 'jstransformers')
+        .end(function (error, res) {
+          if (error) {
             err = error
-          } else if (res.statusCode !== 200) {
-            err = new Error(
-              'Status code: ' + res.statusCode + '\n' + data.toString()
-            )
+            err.message += '\n' + res.text
           } else {
-            obj = JSON.parse(data.toString())
+            obj = res.body
             obj.sort(function (a, b) {
               return (a.name > b.name) - (a.name < b.name)
             })
           }
           rw()
           cb(err, obj)
-        }))
-      })
+        })
     })
     rr()
   })
