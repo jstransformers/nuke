8f1e0f864a555539c62594a4da751c47bdd6e800
Timothy Gu <timothygu99@gmail.com>
2015-04-03 10:42:25 -0700

    repos: Use github-repositories instead
    Caching also removed as it's lame.

--- repos.js
+++ repos.js
@@ -4,47 +4,13 @@
  * MIT-licensed
  */
 
-var request = require('superagent')
-  , bl = require('bl')
-  , ReadWriteLock = require('rwlock')
-  , lock = new ReadWriteLock()  // Make sure the data is cached
-  , obj
-  , err
+var github = require('github-repositories')
 
 module.exports = function (cb) {
-  lock.readLock(function (rr) {
-    if (obj || err) {
-      cb(err, obj)
-      return rr()
-    }
-    lock.writeLock(function (rw) {
-      request
-        .get('https://api.github.com/orgs/jstransformers/repos?per_page=200')
-        // https://developer.github.com/v3/#user-agent-required:
-        //
-        // > All API requests MUST include a valid `User-Agent` header.
-        // > Requests with no `User-Agent` header will be rejected. We
-        // > request that you use your GitHub username, or the name of your
-        // > application, for the `User-Agent` header value. This allows us
-        // > to contact you if there are problems.
-        .set('User-Agent', 'jstransformers')
-        .end(function (error, res) {
-          if (error) {
-            err = error
-            err.message += '\n' + res.text
-          } else {
-            obj = res.body
-                    .filter(function (a) {
-                      return a.name.indexOf('jstransformer') === 0
-                    })
-                    .sort(function (a, b) {
-                      return (a.name > b.name) - (a.name < b.name)
-                    })
-          }
-          rw()
-          cb(err, obj)
-        })
-    })
-    rr()
+  github('jstransformers', function (err, data) {
+    if (err) return cb(err, data)
+    cb(null, data.filter(function (a) {
+               return a.name.indexOf('jstransformer') === 0
+             }))
   })
 }
